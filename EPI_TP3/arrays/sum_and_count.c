5e0cacaf5c5a08daa12950961426ea1b681c9d4b
moussmousse <anthonymousset@hotmail.fr>
2017-09-24 19:02:24 +0200

    sum_and_count is finished

--- EPI_TP3/arrays/sum_and_count.c
+++ EPI_TP3/arrays/sum_and_count.c
@@ -5,7 +5,7 @@
 int sum_of_divisors(int x, size_t *count);
 
 int main(void){
-	int x = 28;
+	int x = 27;
 	size_t count;
 	int res;
 	res = sum_of_divisors(x, &count);
@@ -17,7 +17,20 @@ int main(void){
 
 
 int sum_of_divisors(int x, size_t *count){
-
-
-
+	*count = 0;
+	if (x == 0 || x == 1){
+		*count = 1;
+		return 1;
+	}
+	
+	int k = x-1;
+	int res = 0;
+	while (k>0){
+		if (x%k==0){
+			res+=k;
+			*count = *count +1;
+		}
+		k--;
+	}
+	return res;
 }
