04facdb91c5f22682af1d64adebcfd7da532ee53
moussmousse <anthonymousset@hotmail.fr>
2017-09-24 20:13:09 +0200

    some problems in array_sum

--- EPI_TP3/arrays/basic_op.c
+++ EPI_TP3/arrays/basic_op.c
@@ -3,16 +3,33 @@
 
 
 int sum_of_divisors(int x, size_t *count);
+int array_sum(int *begin, int *end);
+
 
 int main(void){
+	/*
 	int x = 27;
 	size_t count;
 	int res;
 	res = sum_of_divisors(x, &count);
 	printf("res = %d\n", res);
 	printf("count = %zu\n", count);
+	*/
 
+	int array[10] = {1,2,3,4,5,6,7,8,9,10};
+	int *begin = array;
+	int *end = begin +10;
+	int res = array_sum(&begin,&end);
+	printf ("array sum = %d (sum of 1+2+3+4+5+6+7+8+9+10)\n",res);
 	return 0;
+}
+
+int array_sum(int *begin, int *end){
+	int res = 0;
+	for(int i = 0;i < (begin - end); i++){
+		res += *(begin + i);
+	}
+	return res;	
 }
 
 
