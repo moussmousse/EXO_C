90a81f4d62fa4419f740c57ef810be1b9b9d9535
moussmousse <anthonymousset@hotmail.fr>
2017-09-27 17:27:42 +0200

    insert_sort doesn't work

--- EPI_TP3/arrays/insert_sort.c
+++ EPI_TP3/arrays/insert_sort.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void array_shift(int *begin, int *end);
+void array_insert(int *begin, int *end, int x);
+void array_insert_bin(int *begin, int *end, int x);
+void array_insert_sort(int *begin, int *end);
+void array_copy(int *dst, int *begin, int *end);
+void print_array(int *begin, int *end);
 
+int main (){
+  int array[14]={1,9,5,4,7,8,6,2,7,3,6,8,99,1};
+  int *begin=array;
+  int *end=begin+14;
+  printf("------Array to sort------\n");
+  print_array(begin,end);
+  printf("\n----array insert sort----\n");
+  array_insert_sort(begin,end);
+  print_array(begin,end);
+
+}
+
+void print_array(int *begin, int *end){
+  for(;begin<end;++begin)
+    printf("%d",*begin);
+  printf("\n");
+}
 
 void array_rshift(int *begin, int *end){
   end--;
@@ -21,7 +44,9 @@ void array_insert(int *begin, int *end, int x){
 }
 
 void array_insert_bin(int *begin, int *end, int x){
-  int left = begin; int right = end; int mid = begin + (end-begin)/2;
+  int *left = begin;
+  int *right = end;
+  int *mid = begin + (end-begin)/2;
   while (left != right && *mid != x && left != mid){
     if (*mid > x)
       left = mid;
@@ -33,15 +58,21 @@ void array_insert_bin(int *begin, int *end, int x){
   *mid = x;
 }
 
+void array_copy (int *dst, int *begin, int *end){
+  for(;begin<end;++begin)
+    *(dst++)=*begin;
+}
+
 void array_insert_sort(int *begin, int *end){
   int *tab = malloc(sizeof(int)*(end-begin));
   int *endtab = tab + (end-begin);
   *tab=*begin;
-  begin++
+  begin++;
   for(;begin<end;++begin){
-    array_insert(tab,endtab,*begin); // Vérifier la comparaison dans
-  }				     // entre NULL & int in insert
+    array_insert(tab,endtab,*begin); // Vérifier la comparaison
+    print_array(begin,end);
+  }
+  array_copy(end,tab,endtab);
 }
 
 
-void array_insert_sort_bin(
