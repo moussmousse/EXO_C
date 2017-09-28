51cba9f8b0bd1e339b87ce5ad79dc57abe2e1651
moussmousse <anthonymousset@hotmail.fr>
2017-09-28 18:47:05 +0200

    list_reverse is not implemented

--- EPI_TP4/list/list.c
+++ EPI_TP4/list/list.c
@@ -1,2 +1,75 @@
 #include <stdio.h>
-#include <stdbil.h>
+#include <stdlib.h>
+
+#include "list.h"
+
+
+
+void list_init(struct list *list)
+{
+  list->next = NULL;
+}
+
+
+int list_is_empty(struct list *list)
+{
+  return list->next == NULL;
+}
+
+size_t list_len(struct list *list)
+{
+  size_t len = 0;
+  struct list *curent = list;
+  for (;curent->next != NULL; ++curent)
+    len ++;
+  free(curent);
+  return len;
+}
+
+
+void list_push_front(struct list *list, struct list *elm)
+{
+  struct list *tmp = list;
+  list=elm;
+  elm->next=tmp->next;
+  free(tmp);
+}
+
+struct list* list_pop_front(struct list *list)
+{
+  if (list_is_empty(list))
+      return NULL;
+  struct list *tmp = list->next;
+  list->next->next = list->next;
+  return tmp;
+}
+
+struct list* list_find(struct list *list, int value)
+{
+  struct list *tmp = list;
+  for(;tmp->next != NULL; ++tmp)
+  {
+    if (tmp->data == value)
+      return tmp;
+  }
+  return NULL;
+}
+
+
+int list_is_sorted(struct list *list)
+{
+  struct list *tmp = list;
+  tmp=tmp->next;
+  for(;tmp->next != NULL; ++tmp)
+    {
+      if(tmp->data<tmp->next->data)
+	return 0;
+    }
+  return 1;
+}
+
+
+void list_rev(struct list *list)
+{
+
+}
